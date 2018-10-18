fec69c138bea5b668b49a5d0881a6e619c2089cc
Sreetam Das <sreetamdas@gmail.com>
2018-10-18 17:28:33 +0530

    trying #fff bg

--- src/components/App.js
+++ src/components/App.js
@@ -10,6 +10,8 @@ class App extends React.Component {
 		super();
 
 		this.position = this.position.bind(this);
+		this.allLines = this.allLines.bind(this);
+
 		this.state = {
 			coordinates: null,
 			x1: null,
@@ -25,7 +27,7 @@ class App extends React.Component {
 		// console.log("id:", data.node.firstChild.id);
 		const id = data.node.firstChild.id;
 		const el = document.getElementById(id);
-		el.classList.add("yellow");
+		// el.classList.add("yellow");
 		// console.log(id, el.className);
 
 		if (!el) {
@@ -39,8 +41,8 @@ class App extends React.Component {
 		}
 		const box = el.getBoundingClientRect();
 		console.log({ box });
-		const x1 = box.left + 25;
-		const y1 = box.bottom - 25;
+		const x1 = box.left + box.width / 2;
+		const y1 = box.bottom - box.height / 2;
 		const index = this.state.nodes.indexOf(id);
 
 		this.setState({
@@ -61,34 +63,39 @@ class App extends React.Component {
 	newLine = () => {
 		document.addEventListener("click", this.handleClick, false);
 	};
+
+	allLines = () => {
+		// for()
+	};
+
 	render() {
 		return (
 			<div>
 				<h1>This is App.</h1>
 				<Draggable onDrag={this.position}>
-					<div id="e1" style={{ backgroundColor: "" }}>
-						<FontAwesomeIcon id="E1" style={{ backgroundColor: "" }} icon={faDesktop} size="3x" />
+					<div>
+						<FontAwesomeIcon id="E1" icon={faDesktop} size="3x" style={{ backgroundColor: "white" }} />
 					</div>
 				</Draggable>
 				<h1>iuhweifubwiefciuweciuwefcuwbfciwefcwefc</h1>
 				<Draggable onDrag={this.position}>
-					<div id="E2">
-						<FontAwesomeIcon id="" icon={faDesktop} size="3x" />
+					<div>
+						<FontAwesomeIcon id="E2" icon={faDesktop} size="3x" />
 					</div>
 				</Draggable>
 				<Draggable onDrag={this.position}>
-					<div id="e3" style={{ backgroundColor: "" }}>
-						<FontAwesomeIcon id="E3" style={{ backgroundColor: "" }} icon={faDesktop} size="3x" />
+					<div>
+						<FontAwesomeIcon id="E3" icon={faDesktop} size="3x" />
 					</div>
 				</Draggable>
 				<h1>iuhweifubwiefciuweciuwefcuwbfciwefcwefc</h1>
 				<Draggable onDrag={this.position}>
-					<div id="E4">
-						<FontAwesomeIcon id="" icon={faDesktop} size="3x" />
+					<div>
+						<FontAwesomeIcon id="E4" icon={faDesktop} size="3x" />
 					</div>
 				</Draggable>
 				<button onClick={this.newLine}>New Line</button>
-				line followsss:
+				line followssssssss:
 				{this.state.active && (
 					<Line
 						x0={this.state.x0}
