3364b152cfea96b9317e7773e346d80643d9afe2
Sreetam Das <sreetamdas@gmail.com>
2018-10-31 15:16:40 +0530

    export default

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { ReactLines } from "./components/App";
+import ReactLines from "./components/App";
 import "./index.css";
 
 ReactDOM.render(<ReactLines />, document.getElementById("root"));
